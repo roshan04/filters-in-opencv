d85b49c8fd24040357748df8bfe9edc1d0f510a7
ROSHAN KISHOR NIKAM <47280057+roshan04@users.noreply.github.com>
2019-02-20 12:47:44 +0530

    Add files via upload

---
+++ filters.py
@@ -0,0 +1,14 @@
+import cv2
+img=cv2.imread("cv.tif",1)
+#img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+box=cv2.boxFilter(img,-1,(4,4))
+blur=cv2.blur(img,(4,4))
+gaus=cv2.GaussianBlur(img,(5,5),0)
+cv2.imshow("original",img)
+cv2.imshow("output1",box)
+cv2.imshow("output2",blur)
+cv2.imshow("output3",gaus)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
